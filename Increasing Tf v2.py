d5148f930d9010332c9f874e3bf47f996e685450
Theo Hancock <123842460+Theo-Hancock@users.noreply.github.com>
2025-02-26 12:53:01 +0000

    Add files via upload

--- Increasing Tf v2.py
+++ Increasing Tf v2.py
@@ -22,7 +22,7 @@ r = delta_t/delta_x**2   #alpha*delta_t/delta_x**2, need r = 1/2 for system to b
 k = Lambda*delta_t       #beta*delta_t
 
 #Tf = Qt, Q > 0 constant
-Q = 5
+Q = 2.8701
 
 #Boundary conditions at t=0
 left_boundary_condition = 0
@@ -49,10 +49,7 @@ def update_state(time_gap):
     new_state = []
     new_state.append(left_boundary_condition)
     for i in range(1,number_of_points-1):
-        if i == number_of_points-2:
-            new_state.append(r*state[i-1] + r*right_boundary_condition + (1-2*r)*state[i] + k*math.exp(state[i]))
-        else:
-            new_state.append(r*state[i-1] + r*state[i+1] + (1-2*r)*state[i] + k*math.exp(state[i]))
+        new_state.append(r*state[i-1] + r*state[i+1] + (1-2*r)*state[i] + k*math.exp(state[i]))
     new_state.append(right_boundary_condition)
     if counter%time_gap == 0:
         plotsplease.append((new_state, counter))
@@ -61,44 +58,109 @@ def update_state(time_gap):
     return state
 
 #Simulate the system
-for l in range(1,50001):
+for l in range(1,24001):
     counter += 1
     left_boundary_condition += Q*delta_t
     right_boundary_condition += Q*delta_t
-    update_state(15000)
+    update_state(8000)
     
-for l in range(1,8001):
+for l in range(1,1001):
     counter += 1
     left_boundary_condition += Q*delta_t
     right_boundary_condition += Q*delta_t
-    update_state(58000)
+    update_state(1000)
+    
+for l in range(1,41):
+    counter += 1
+    left_boundary_condition += Q*delta_t
+    right_boundary_condition += Q*delta_t
+    update_state(40)
+    
+for l in range(1,10):
+    counter += 1
+    left_boundary_condition += Q*delta_t
+    right_boundary_condition += Q*delta_t
+    update_state(8)
 
-for l in range(1,272):
+for l in range(1,3):
     counter += 1
     left_boundary_condition += Q*delta_t
     right_boundary_condition += Q*delta_t
-    update_state(100)
+    update_state(25051)  
+    
 
 
 #Plot time evolution of system
 x = np.linspace(-1, 1, number_of_points)
 
 for m in range(0,len(plotsplease)):
-    plt.plot(x,plotsplease[m][0], label = f'τ = {round(plotsplease[m][1]/10000, 2)}')
-plt.plot(x, state, label = f'τ = {round(counter/10000,3)}')
+    plt.plot(x,plotsplease[m][0], label = f'τ = {round(plotsplease[m][1]/10000, 4)}')
+#plt.plot(x, state, label = f'τ = {round((counter)/10000,4)}')
 plt.xlabel("x")
 plt.ylabel("u")
-plt.ylim(0,30)
+plt.ylim(0,40)
 plt.legend()
 plt.show()
 
+#Save results for Q=3 to separate array
+plotsplease_dupe = tuple(plotsplease)
 
 #Plot final state
-plt.plot(x, state, label = f'τ = {counter}')
-plt.ylim(0,30)
+plt.plot(x, state, label = f'τ = {round((counter)/10000, 3)}')
+#plt.ylim(0,30)
 plt.legend()
 plt.show()
 
+#Save final state for Q = 2.86 to separate array
+state_dupe = (tuple(state), counter-1)
 
 #Time to blowup for different values of Q (and Lambda=0.01):
-    #Q = 1:  58273 (central blowup)
+    #Q = 1:     58273 (central blowup)
+    #Q = 5:     16414 (two blowups)
+    #Q = 3:     24233 (two blowups)
+    #Q = 2.8    25521 (central blowup)
+    #Q = 2.9    24857 (two blowups)
+    #Q = 2.87   25052 (two blowups)
+    #Q = 2.88   24987 (two blowups)
+    #Q = 2.86   25118 (central blowup)
+    
+#Bifurcation at some point between 2.86 and 2.87
+
+
+
+#Create plot with two subplots to compare Q = 3 and Q = 2.88
+fig, (ax1, ax2) = plt.subplots(2, figsize = (8,8))
+for m in range(0,len(plotsplease_dupe)):
+    ax1.plot(x,plotsplease_dupe[m][0], label = f'τ = {round(plotsplease_dupe[m][1]/10000, 3)}')
+ax1.set_xlabel("x")
+ax1.set_ylabel("u")
+ax1.set_ylim(0,30)
+for m in range(0,len(plotsplease)):
+    ax2.plot(x,plotsplease[m][0], label = f'τ = {round(plotsplease[m][1]/10000, 4)}')
+ax2.set_xlabel("x")
+ax2.set_ylabel("u")
+ax2.set_ylim(0,40)
+ax1.set_title('Q = 3')
+ax2.set_title('Q = 2.87')
+plt.tight_layout()
+ax1.legend()
+ax2.legend()
+ax1.grid()
+ax2.grid()
+
+
+#Create plot with two subplots to compare Q = 2.87 and Q = 2.88
+fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize = (16,6))
+ax1.plot(x,state_dupe[0], label = f'τ = {round(state_dupe[1]/10000, 3)}')
+ax1.set_xlabel("x")
+ax1.set_ylabel("u")
+ax2.plot(x, state, label = f'τ = {round((counter-1)/10000, 3)}')
+ax2.set_xlabel("x")
+ax2.set_ylabel("u")
+ax1.set_title('Q = 2.86')
+ax2.set_title('Q = 2.87')
+plt.tight_layout()
+ax1.legend()
+ax2.legend()
+ax1.grid()
+ax2.grid()
