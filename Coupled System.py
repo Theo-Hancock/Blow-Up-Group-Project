73fdb3ca07903ca4611f910857113561fa5a4fc5
Theo Hancock <123842460+Theo-Hancock@users.noreply.github.com>
2025-03-05 12:51:24 +0000

    Add files via upload

--- Coupled System.py
+++ Coupled System.py
@@ -17,10 +17,10 @@ delta_t = 1/10000
 D_T = 1             #Diffusion constant - temperature
 D_a = 1             #Diffusion constant - reactant
 D = D_a/D_T
-gamma = 1           #Reaction constant - temperature
-mu = 1              #Reaction constant - reactant
-r_T = delta_t/delta_x**2
-r_a = D * (delta_t/delta_x**2)
+gamma = 5           #Reaction constant - temperature
+mu = 5              #Reaction constant - reactant
+r_T = delta_t/delta_x**2            
+r_a = D * (delta_t/delta_x**2)      #Make sure r_T and r_a are both < 0.5
 
 
 #Initial amount of reactant
@@ -51,12 +51,16 @@ for s in range(0, number_of_points):
     initial_reactant.append(initial_a)
 initial_reactant = tuple(initial_reactant) #To prevent initial state from being modified
 
-#Run this block and set the boundary conditions if needed to reset the system
+#Run this block (and set the boundary conditions if needed) to reset the system
 T_state = list(initial_temp)
 a_state = list(initial_reactant)
 time = 0
 plotsplease=[]
 plotsplease.append((list(initial_reactant), list(initial_temp), time))
+central_values=[[],[],[]]
+central_values[0].append(initial_reactant[50])
+central_values[1].append(initial_temp[50])
+central_values[2].append(0)
 
 
 #Update reactant concentration each timestep
@@ -70,7 +74,6 @@ def update_a_state():
     for d in range(0,number_of_points):
         a_state[d] = new_state[d]
     return a_state
-
                  
 #Update temperature each timestep                
 def update_T_state():
@@ -84,6 +87,7 @@ def update_T_state():
         T_state[d] = new_state[d]
     return T_state
 
+
 #Combined function to update reactant and temperature, add plots
 def update_state(n_steps, time_gap):
     for l in range(0,n_steps):
@@ -95,23 +99,77 @@ def update_state(n_steps, time_gap):
         time += 1
         if time % time_gap == 0:
             plotsplease.append((list(a_entry), list(T_entry), time))
+        if time % (1/(1000*delta_t)) == 0:
+            central_values[0].append(a_entry[50])
+            central_values[1].append(T_entry[50])
+            central_values[2].append(time*delta_t)
+                
             
             
 #Simulate the system
-update_state(20000,4000)
+update_state(100000,10000)
+
+#Save results (for making plots with subplots for different parameter values)
+central_values_save = tuple(central_values)
+plotsplease_save = tuple(plotsplease)
 
 
 #Plot time evolution of system
 x = np.linspace(-1, 1, number_of_points)
 
-for m in range(0,len(plotsplease)):
-    plt.plot(x,plotsplease[m][0], label = f'Reactant τ = {round(plotsplease[m][2]/10000, 4)}')
-for m in range(0,len(plotsplease)):
-    plt.plot(x,plotsplease[m][1], label = f'Temperature τ = {round(plotsplease[m][2]/10000, 4)}')
-plt.xlabel("x")
-#plt.ylim(0,5)
-plt.legend()
-plt.show()
-    
 
+#Plot with two subplots for different time values
+fig, (ax1, ax2) = plt.subplots(2, figsize = (8,8))
+ax1.plot(x, plotsplease[1][0], label = 'Reactant a')
+ax1.plot(x, plotsplease[1][1], label = 'Temperature u')
+ax1.set_xlabel("x")
+ax1.set_title(f'τ = {round(plotsplease[1][2]*delta_t, 2)}')
+ax2.plot(x, plotsplease[2][0], label = 'Reactant a')
+ax2.plot(x, plotsplease[2][1], label = 'Temperature u')
+ax2.set_xlabel("x")
+ax2.set_title(f'τ = {round(plotsplease[2][2]*delta_t, 2)}')
+plt.tight_layout()
+ax1.legend()
+ax2.legend()
+ax1.grid()
+ax2.grid()
+
+
+#Plot central temperature and reactant over time
+plt.plot(central_values[2], central_values[0], label = 'Reactant a(0,τ)')
+plt.plot(central_values[2], central_values[1], label = 'Temperature u(0,τ)')
+plt.xlabel('τ')
+plt.tight_layout()
+plt.legend()
+plt.grid()
+
+
+#Plots for comparing parameter values
+fig, (ax1, ax2) = plt.subplots(2, figsize = (6,8))
+ax1.plot(central_values_save[2], central_values_save[0], label = 'Reactant a(0,τ)')
+ax1.plot(central_values_save[2], central_values_save[1], label = 'Temperature u(0,τ)')
+ax1.set_xlabel('τ')
+ax1.set_xlim(0,5)
+ax1.set_title('γ = 5, μ = 1')
+ax2.plot(central_values[2], central_values[0], label = 'Reactant a(0,τ)')
+ax2.plot(central_values[2], central_values[1], label = 'Temperature u(0,τ)')
+ax2.set_xlabel('τ')
+ax2.set_xlim(0,5)
+ax2.set_title('γ = 5, μ = 5')
+plt.tight_layout()
+ax1.legend()
+ax2.legend()
+ax1.grid()
+ax2.grid()
+
+
+
+
+
+
+
+
+        
+
+    
 
